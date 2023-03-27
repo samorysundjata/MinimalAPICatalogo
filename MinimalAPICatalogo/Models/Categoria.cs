11d39b69791f5be5e359341c31c882676e7cecf0
Samory Sundjata <samory.sundjata@gmail.com>
2023-03-27 09:06:51 -0300

    Dependências e models.

---
+++ MinimalAPICatalogo/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace MinimalAPICatalogo.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+
+        public string? Nome { get; set; }
+
+        public string? Descricao { get; set; }
+
+        ICollection<Produto> Produtos { get; set; } 
+    }
+}
