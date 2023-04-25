1d57dfee14e177f272940e6d8edb42501f4583e6
Samory Sundjata <samory.sundjata@gmail.com>
2023-04-25 12:03:23 -0300

    Endpoints para produto.

--- MinimalAPICatalogo/Program.cs
+++ MinimalAPICatalogo/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.EntityFrameworkCore;
 using MinimalAPICatalogo.Context;
 using MinimalAPICatalogo.Models;
@@ -65,6 +66,59 @@ app.MapDelete("/categoria/{id:int}", async(int id, AppDbContext db) =>
 
         return Results.NoContent();
     });
+
+//----------------- endpoints para produto ------------
+app.MapPost("/produtos", async(Produto produto, AppDbContext db)
+ =>{
+     db.Produtos.Add(produto);
+     await db.SaveChangesAsync();
+
+     return Results.Created($"/produtos/{produto.ProdutoId}", produto);
+});
+
+app.MapGet("/produtos", async (AppDbContext db) => await db.Produtos.ToListAsync());
+
+app.MapGet("/produtos/{id:int}", async (int id, AppDbContext db) 
+    => { 
+        return await db.Produtos.FindAsync(id)
+                     is Produto produto
+                     ? Results.Ok(produto)
+                     : Results.NotFound();
+    });
+
+app.MapPut("/produtos/{id:int}", async (int id, Produto produto ,AppDbContext db) => 
+{
+        if (produto.ProdutoId != id) return Results.BadRequest("Deu ruim!");
+
+        var produtoDB = await db.Produtos.FindAsync(id);
+
+        if(produtoDB is null) return Results.NotFound("Não achou...");
+
+        produtoDB.Nome = produto.Nome;
+        produtoDB.Descricao = produto.Descricao;
+        produtoDB.Imagem = produto.Imagem;
+        produtoDB.Categoria = produto.Categoria;
+        produtoDB.Estoque = produto.Estoque;
+        produtoDB.DataCompra = produto.DataCompra;
+        produtoDB.CategoriaId = produto.CategoriaId;
+
+        await db.SaveChangesAsync();
+
+        return Results.Ok(produtoDB + " Deu certo!");
+});
+
+app.MapDelete("/produtos/{id:int}", async (int id, AppDbContext db) => 
+{
+    var produto = await db.Produtos.FindAsync(id);
+
+    if(produto is null) return Results.NotFound();
+
+    db.Produtos.Remove(produto);
+    await db.SaveChangesAsync();
+
+    return Results.NoContent();
+    
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
